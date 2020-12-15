484126f1434f0c3ef20762c9f72b87de27070b15
Matthieu Tran <matthieu.tran@cpe.fr>
2020-12-15 09:12:02 +0100

    class

---
+++ objets.py
@@ -0,0 +1,13 @@
+from Tkinter import
+
+class cObjets:
+    def __init_(self,missile,mechant,perso):
+        self.missile=missile
+        self.mechant=mechant
+        self.perso=perso
+
+
+
+
+class cPerso(cObjets):
+
