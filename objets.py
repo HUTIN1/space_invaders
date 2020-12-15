7de0fe90cc20c22c5a707a203134403a32dec9da
Matthieu Tran <matthieu.tran@cpe.fr>
2020-12-15 09:44:47 +0100

    merde

--- objets.py
+++ objets.py
@@ -1,13 +1,39 @@
 from Tkinter import
 
 class cObjets:
-    def __init_(self,missile,mechant,perso):
-        self.missile=missile
-        self.mechant=mechant
-        self.perso=perso
+    def __init_(self,largeurX,largeurY,tags,image,posX,posY,canvas):
+        self.largeurX=largeurX
+        self.largeurY=largeurY
+        self.tags=tags
+        self.image=image
+        self.posX=posX
+        self.posY=posY
+        self.canevas=canevas
 
 
 
 
 class cPerso(cObjets):
+    def fBouger(self):
+        touche=event.keysym
+
+        if touche == 'q':
+            PosX -= 20
+
+        if touche == 'd':
+            PosX += 20
+    
+    def fTirer(self):
+
+
+
+class cMechant(cObjets):
+
+class cMissile(cObjets):
+    def fDeplacement(self):
+        posY=posY-20
+        
+
+
+
 
