5a7569ec67711338590e5631b6f848d0f479af9c
Matthieu Tran <matthieu.tran@cpe.fr>
2020-12-15 11:14:11 +0100

    .move

--- objets.py
+++ objets.py
@@ -5,11 +5,11 @@ class cObjets:
         self.__largeurX=largeurX
         self.__largeurY=largeurY
         self.__tags=tags
-        self.__image=image
         self.__posX=posX
-        self.__posY=posY
+        self.__posY=posY        
+        self.__vitesse=vitesse
         self.__canevas=canevas
-
+        self.__image=self.__canevas.create_image(self.__posX,self.__posY,image=image)
 
 
 
@@ -18,10 +18,16 @@ class cPerso(cObjets):
         touche=event.keysym
 
         if touche == 'q': #Déplacement de notre vaisseau à gauche
-            self.posX -= 20
+            self.__vitesse = -20
+            self.__posX -= 20
+            self.__canevas.move(self.__image,self.__vitesse,0)
+  
 
         if touche == 'd': #Déplacement de notre vaisseau à gauche
-            self.posX += 20
+            self.__vitesse = 20
+            self.__posX += 20
+            self.__canevas.move(self.__image,self.__vitesse,0)
+  
 
         #Problème avec appel de cMissile (on veut créer plusieurs missiles).      
     def fTirer():
@@ -32,32 +38,33 @@ class cMechant(cObjets):
     def __init__(self,cote):
         self.__cote=cote
 
+    def fDeplacement_mechant(self):
+        if self.__cote==1: #1=le méchant se déplace vers la droite
+            self.__vitesse = 20
+            self.__posX += 20           
+            self.__canevas.move(self.__image,self.__vitesse,0)
 
-    def fDeplacement_mechant():
-        if cote==1: #1=le méchant se déplace vers la droite
-            self.posX=self.posX+20
-        elif cote==0: #2=le méchant se déplace vers la gauche
-            self.posX==self.posX-20
-        elif cote==2: #3=le méchant descend d'un étage
-            self.posY==self.posY+20
-
-        
+        elif self.__cote==0: #2=le méchant se déplace vers la gauche
+            self.__vitesse =  -20      
+            self.__posX -= 20
+            self.__canevas.move(self.__image,self.__vitesse,0)
 
 
+        elif self.__cote==2: #3=le méchant descend d'un étage
+            self.__vitesse = 20
+            self.__posY += 20
+            self.__canevas.move(self.__image,0,self.__vitesse)
 
 
 
 class cMissile(cObjets):
-    def __init_(self):
-        self.fIm()
-    
     def fDeplacement_missile(self):
-        self.posY=self.posY-20
+        self._vitesse = -20
+        self.__posY -= 20
+        self.__canevas.move(self.__image,0,self.__vitesse)
 
-#Im= Image missile
-    def fIm(self):
-        canevas.create_image(self.posX,self.posY)
         
+
 
 
 
