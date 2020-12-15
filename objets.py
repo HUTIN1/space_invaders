20ee6b61d7f43a79a25fb6417ecf26a492341e91
Matthieu Tran <matthieu.tran@cpe.fr>
2020-12-15 10:24:55 +0100

    coucou

--- objets.py
+++ objets.py
@@ -2,13 +2,13 @@ from Tkinter import
 
 class cObjets:
     def __init_(self,largeurX,largeurY,tags,image,posX,posY,canvas):
-        self.largeurX=largeurX
-        self.largeurY=largeurY
-        self.tags=tags
-        self.image=image
-        self.posX=posX
-        self.posY=posY
-        self.canevas=canevas
+        self.__largeurX=largeurX
+        self.__largeurY=largeurY
+        self.__tags=tags
+        self.__image=image
+        self.__posX=posX
+        self.__posY=posY
+        self.__canevas=canevas
 
 
 
@@ -17,21 +17,46 @@ class cPerso(cObjets):
     def fBouger(self):
         touche=event.keysym
 
-        if touche == 'q':
-            PosX -= 20
+        if touche == 'q': #Déplacement de notre vaisseau à gauche
+            self.posX -= 20
 
-        if touche == 'd':
-            PosX += 20
-    
-    def fTirer(self):
+        if touche == 'd': #Déplacement de notre vaisseau à gauche
+            self.posX += 20
 
+        #Problème avec appel de cMissile (on veut créer plusieurs missiles).      
+    def fTirer()
+    tirer=cMissile()
 
 
 class cMechant(cObjets):
+    def __init__(self,cote):
+        self.__cote=cote
+
+
+    def fDeplacement_mechant():
+        if cote==1: #1=le méchant se déplace vers la droite
+            self.posX=self.posX+20
+        elif cote==0: #2=le méchant se déplace vers la gauche
+            self.posX==self.posX-20
+        elif cote==2: #3=le méchant descend d'un étage
+            self.posY==self.posY+20
+
+        
+
+
+
+
 
 class cMissile(cObjets):
-    def fDeplacement(self):
-        posY=posY-20
+    def __init_(self):
+        self.fIm()
+    
+    def fDeplacement_missile(self):
+        self.posY=self.posY-20
+
+#Im= Image missile
+    def fIm(self):
+        canevas.create_image(self.posX,self.posY)
         
 
 
