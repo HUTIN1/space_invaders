93be311c91eca0169171b4139e4ae4a33e018365
Matthieu Tran <matthieu.tran@cpe.fr>
2021-01-12 12:01:21 +0100

    l

--- fenetre.py
+++ fenetre.py
@@ -22,58 +22,66 @@ from tkinter import Tk, Canvas, Button, Label, IntVar
 
 
 class fenetre(Tk):
-    # Initialisation de la fenêtre
+    #Initialisation de la fenêtre
     def __init__(self,largeur_x_mechant,largeur_y_mechant,largeur_x_missile,largeur_y_missile,
                  largeur_x_perso,largeur_y_perso,freq_missile,freq_mechant,
                  pas_mechant,pas_missile,pas_perso,proba_missile_mechant,nb_mechant,
                  nb_ligne_mechant,largeur_blocks,hauteur_blocks,nbgrosblocks,nbligneblocks,
                  nbcolonneblocks):
         Tk.__init__(self)
+        #Initialisation méchant
         self.__mechant={} #Dictionnaire contenant tous les méchants
-        self.__largeur_x_mechant=largeur_x_mechant
+        self.__largeur_x_mechant=largeur_x_mechant #Hitxbox méchant
         self.__largeur_y_mechant=largeur_y_mechant
-        self.__perso=None #Création de notre vaisseau
-        self.__missiles={} #Dictionnaire contenant tous les missiles
-        self.__blocks={} #Dictionniaire contenant les blocks permettant au vaisseau de se sacher
         self.__image_mechant=None
+        self.__freq_mechant=freq_mechant #Fréquence à laquelle le méchant bouge (en ms)
+        self.__nb_mechant=nb_mechant
+        self.__nb_ligne_mechant=nb_ligne_mechant
+        self.__proba_missile_mechant=proba_missile_mechant
+        self.__pas_mechant=pas_mechant
+         
+        #Initialisation perso (=vaisseau)
+        self.__missiles={} #Dictionnaire contenant tous les missiles
+        self.__perso=None #Création de notre vaisseau
         self.__image_perso=None
+        self.__largeur_x_perso=largeur_x_perso #Hitxbox vaisseau
+        self.__largeur_y_perso=largeur_y_perso
+        self.__nbvie=3
+        self.__pas_perso=pas_perso
+
+        #Initialisation missiles
         self.__image_missile=None
+        self.__largeur_x_missile=largeur_x_missile #Hitbox missile
+        self.__largeur_y_missile=largeur_y_missile
+        self.__freq_missile=freq_missile #Fréquence à laquelle les missiles bougent (en ms)
+        self.__pas_missile=pas_missile
+        self.__nb_tire=0
+
+        #Initialisation blocks (permettant au personnage de se protéger des missiles ennemis)
+        self.__blocks={} #Dictionniaire contenant les blocks
+        self.__largeur_blocks=largeur_blocks ; #Hitbox block
+        self.__hauteur_blocks=hauteur_blocks
+        self.__nbgrosblocks=nbgrosblocks #Nombre de "paquet" de blocks 
+        self.__nbligneblocks=nbligneblocks #Nombre de blocks par "paquet"
+        self.__nbcolonneblocks=nbcolonneblocks
+
         self.__image_gameover=None
         self.__image_coeur=None
         self.__image_win=None
-        self.__score=IntVar() 
-        self.__largeur_x_missile=largeur_x_missile
-        self.__largeur_y_missile=largeur_y_missile
-        self.__largeur_x_perso=largeur_x_perso
-        self.__largeur_y_perso=largeur_y_perso
-        self.__freq_missile=freq_missile
-        self.__freq_mechant=freq_mechant
-        self.__pas_mechant=pas_mechant
-        self.__pas_missile=pas_missile
-        self.__pas_perso=pas_perso
-        self.__proba_missile_mechant=proba_missile_mechant
-        self.__nb_mechant=nb_mechant
-        self.__nb_ligne_mechant=nb_ligne_mechant
-        self.__largeur_blocks=largeur_blocks
-        self.__hauteur_blocks=hauteur_blocks
-        self.__nbgrosblocks=nbgrosblocks
-        self.__nbligneblocks=nbligneblocks
-        self.__nbcolonneblocks=nbcolonneblocks
-        self.__nbvie=3
-        self.__image_fond=None
-        self.__nb_tire=0
+        self.__score=IntVar()
         self.creer_widget()
 
+    """Fonction permetttant de créer les widgets de la fenêtre. Le bouton 'Game' sert à lancer/relancer une partie,
+     le bouton 'quitter' sert à quitter et le label permet de gérer le score"""
 
-        #Fonction permet de créer les widget du canvas ainsi que des boutons pour quitter et lancer le jeu.
     def creer_widget(self):
         self.canvas=Canvas(self,width=500,height=500,bg="black")
         self.canvas.place(x=0,y=0)
         
-        self.quitter=Button(self,text="quitter",command=self.destroy,activebackground="red")
+        self.quitter=Button(self,text="quitter",command=self.destroy,activeforeground="red")
         self.quitter.place(x=550,y=300)
         
-        self.game=Button(self,text="Game",command=self.start,activebackground="red")
+        self.game=Button(self,text="Game",command=self.start,activeforeground="red")
         self.game.place(x=550,y=200)
 
         self.__score.set(0)
@@ -82,7 +90,8 @@ class fenetre(Tk):
 
         
 
-        #Fonction permettant de définir les images et les attribuer à la classe
+    """Fonction permettant de définir les images et les attribuer à la classe
+    Les variables 'mechant, perso, missile, gameover, coeur et win' sont les images .gif définies dans principal.py"""
     def setimage(self,mechant,perso,missile,gameover,coeur,win):
         self.__image_mechant=mechant
         self.__image_perso=perso
@@ -90,11 +99,13 @@ class fenetre(Tk):
         self.__image_gameover=gameover
         self.__image_coeur=coeur
         self.__image_win=win
-        self.__perso=cPerso(self.__largeur_x_perso,self.__largeur_y_perso,
-                            self.__image_perso,400,450,self.canvas,self,self.__pas_perso)
+        #Le vaisseau est initialisé une seul fois et n'est jamais supprimé contrairement aux méchants,missiles et blocks.
+        self.__perso=cPerso(self.__largeur_x_perso,self.__largeur_y_perso, 
+                            self.__image_perso,400,450,self.canvas,self,self.__pas_perso) 
 
 
-        #Fonction permettant de créer un missile et de l'ajouter au dictionnaire self.__missiles
+    """Fonction permettant de créer un missile et de l'ajouter au dictionnaire self.__missiles.
+    Les variables posx et posy donnent la position au missile crée et camp permet de définir si le missile provient d'un méchant ou du vaisseau"""
     def settirer(self,posx,posy,camp):
         pas=self.__pas_missile
         if camp=="perso":
@@ -104,16 +115,17 @@ class fenetre(Tk):
         self.__nb_tire+=1
     
         
-        #Fonction permettant de lancer le programme lorsqu'on appuie sur le bouton "game", il crée les méchants et notre vaisseau
+    """Fonction permettant de lancer/relancer le jeu lorsqu'on appuie sur le bouton "game", il reinitialise puis crée les méchants et les 
+    vies de notre vaisseau. Il permet aussi d'enlever l'affichage de Victoire/Défaite et de reinitialiser le score """
     def start(self):
         self.__mechant={}
-        self.after(self.__freq_mechant,self.fAllmechant)
-        X=100
+        self.after(self.__freq_mechant,self.fAllmechant) #Permet de gérer le déplacement du "block" de méchant (lorsque tous les méchants change de ligne par exemple)
+        X=100 #Position du premier méchant de la premier ligne
         Y=100
         a=0
         b=self.__nb_mechant//self.__nb_ligne_mechant
-        if self.__nb_mechant%self.__nb_ligne_mechant >= 1:
-            a=1
+        if self.__nb_mechant%self.__nb_ligne_mechant >= 1: #Ces boucles 'for' permettent de créer les méchants en fonction du nombre de ligne
+            a=1                                            # et du nombre de méchants définis dans principal.py
         for j in range(self.__nb_ligne_mechant+a):
             X=100
             if j<=self.__nb_ligne_mechant-1:
@@ -131,17 +143,20 @@ class fenetre(Tk):
                                     self.__freq_mechant,self.__proba_missile_mechant)
                     X=X+35
                 Y+=50
+
         self.canvas.delete('image_gameover')
         self.canvas.delete('image_win')
         self.__nbvie=3
         self.__score.set(0)
         poscoeur=10
-        for i in range (self.__nbvie):
+
+        for i in range (self.__nbvie): #Cette boucle permet de supprimer les images de nos vies en fonction du nombre de vie
             poscoeur=poscoeur+40
             self.canvas.create_image(poscoeur,50,image=self.__image_coeur, tags='coeur'+str(i))
-        self.game.configure(state='disabled') 
 
- 
+        self.game.configure(state='disabled') #Désactive le bouton game une fois que la partie est lancée
+
+        #Cette boucle permet de générer les blocks en fonction des paramètres de principal.py
         for i in range(self.__nbgrosblocks):
             xi=i*self.__largeur_blocks*2*self.__nbcolonneblocks
             y=350
@@ -157,7 +172,7 @@ class fenetre(Tk):
     
         
         
-    #Fonction permettant de gérer le déplacement du "bloc" de méchants en particulier lorsque le groupe de méchant doit changer de direction
+    """Fonction permettant de gérer le déplacement du "block" de méchants en particulier lorsque le groupe de méchant doit changer de direction"""
     def  fAllmechant(self):
         a=0
         for i in self.__mechant:
@@ -168,20 +183,22 @@ class fenetre(Tk):
             for i in self.__mechant:
                 self.__mechant[i].fChangecote()
                 self.__mechant[i].fChangeposY()
-        if self.__nbvie!=0:
-            self.after(self.__freq_mechant,self.fAllmechant)
+        if self.__nbvie!=0: 
+            self.after(self.__freq_mechant,self.fAllmechant) #Permet à la fonction de s'appeler elle même pour faire bouger les méchants si on a encore des vies
             self.fVaisseau_touche()
             
             
 
-        #Fonction permettant de finir la partie en cas de défaite en affichant GameOver
+    """Fonction permettant de finir la partie en cas de défaite en affichant GameOver, réactive le bouton 'game' pour pouvoir rejouer
+     et supprime les méchants restant"""
     def fGame_over (self):
             self.game.configure(state='normal')
             for cle in self.__mechant.keys():
                 self.__mechant[cle].setmort()
             self.canvas.create_image(250,250,image=self.__image_gameover, tags='image_gameover')
 
-
+    """Fonction permettant de savoir lorsque le vaisseau est touché par un méchant (et non pas par des missiles méchants)
+    et d'appeler fGame_Over """
     def fVaisseau_touche(self):
         vX,vY=self.__perso.fGet()
         a=0
@@ -235,13 +252,13 @@ class fenetre(Tk):
         if posY_missile<=0 and posY_missile>=500:
                 del self.__missiles[numero_missile]
                 missile.setmort()
-        if self.__mechant=={}:
+
+        if self.__mechant=={}:#Permet d'afficher l'image de victoire de réactiver le bouton 'game'
                     self.canvas.create_image(250,250,image=self.__image_win, tags='image_win')
                     self.game.configure(state='normal')
                     self.__nbvie=0
              
-
-
+    """Fonction permettant d'ajouter 100 000 points à notre score après avoir tué un ennemi"""
     def fScore(self):
         self.__score.set(self.__score.get()+100000)
 
--- objets.py
+++ objets.py
@@ -11,34 +11,34 @@ class cPerso():
         self.__pas=pas
         self.__canevas=canevas
         self.__image=self.__canevas.create_image(self.__posX,self.__posY,image=image, tags="perso")
-        fenetre.bind('<Button-1>',self.fTirer)
+        fenetre.bind('<space>',self.fTirer)
         fenetre.bind('q',self.fBougerGauche)
         fenetre.bind('d',self.fBougerDroite)
 
-       #Fonction permettant de retourner la position du perso 
+    """Fonction permettant de retourner la position du perso """
     def fGet(self):
         return self.__posX,self.__posY
 
-        #Fonction permettant le déplacement de notre vaisseau à gauche
+    """Fonction permettant le déplacement de notre vaisseau à gauche """
     def fBougerGauche(self,event):
         if self.__posX>=30:
             self.__posX -=self.__pas
             self.__canevas.move(self.__image,-self.__pas,0)
 
-        #Fonction permettant le déplacement de notre vaisseau à droite
+    """Fonction permettant le déplacement de notre vaisseau à droite """
     def fBougerDroite(self,event):
         if self.__posX<=470:
             self.__posX += self.__pas
             self.__canevas.move(self.__image,self.__pas,0)
 
-        #Problème avec appel de cMissile (on veut créer plusieurs missiles).      
+    """Fonction permettant au vaisseu de tirer"""    
     def fTirer(self,event):
             self.__fen.settirer(self.__posX,self.__posY,"perso")
 
-
+    """Fonction permettant de supprimer le vaisseau"""
     def setmort(self):
         self.__canevas.delete("perso")
-        print("MortPerso")
+
     
 
         
@@ -47,7 +47,7 @@ class cPerso():
 class cMechant():
     #Initialisation du méchant
     def __init__(self,largeurX,largeurY,tags,image,posX,posY,canevas,fenetre,pas,freq,proba_missile):
-        self.__cote=1
+        self.__cote=1 #Permet de gérer si le méchant bouge à gauche (coté=2) ou à droite (coté=1)
         self.__fen=fenetre
         self.__largeurX=largeurX
         self.__largeurY=largeurY
@@ -59,31 +59,33 @@ class cMechant():
         self.__pas=pas
         self.__freq=freq
         self.__proba=proba_missile
-        self.__vie="vie"
+        self.__vie="vie" #Permet de gérer si le méchant est en vie ou pas (donc son déplacement)
         self.__image=self.__canevas.create_image(self.__posX,self.__posY,image=image, tags="mechant"+tags)
         self.__fen.after(freq,self.fDeplacement_mechant)
         
-        #Fonction permettant de retourner les positions du méchant
+    """Fonction permettant de retourner les positions du méchant"""
     def fGet(self):
         return (self.__posX,self.__posY)
-    
+
+    """Fonction permettant de définir si le méchant est mort"""
     def setmort(self):
         self.__vie="mort" 
 
-        #Fonction permettant de changer le "cote" qui est utilisé dans la fonction fDeplacement_mechant
+    """Fonction permettant de changer le "cote" qui est utilisé dans la fonction fDeplacement_mechant"""
     def fChangecote(self):
         if self.__cote==1:
             self.__cote=2
         else:
             self.__cote=1
 
-        #Fonction permettant de faire descendre les méchants sur l'écran
+    """Fonction permettant de faire descendre les méchants sur l'écran (leur position sur l'axeY)"""
     def fChangeposY(self):
         self.__posY+=self.__pas
         self.__canevas.move(self.__image,0,self.__pas)
 
 
-        #Fonction permettant de faire déplacer les méchants vers la droite/gauche en fonction du coté (cf fChangecote)
+    """Fonction permettant de faire déplacer les méchants vers la droite/gauche en fonction du coté et les tirs des méchants (qui
+    sont aléatoires)"""
     def fDeplacement_mechant(self):
         if self.__cote==1: #1=le méchant se déplace vers la droite
             self.__posX += self.__pas      
@@ -92,7 +94,8 @@ class cMechant():
         elif self.__cote==2: #2=le méchant se déplace vers la gauche    
             self.__posX -= self.__pas
             self.__canevas.move(self.__image,-self.__pas,0)
-            
+
+        #Après chaque déplacement le méchant à 'proba_missile_méchant de tirer' (défini dans principal.py)
         if random()<self.__proba:
             self.__fen.settirer(self.__posX,self.__posY,"mechant")
         if self.__vie=="vie":
@@ -114,20 +117,21 @@ class cMissile():
         self.__vitesse=vitesse
         self.__canevas=canevas
         self.__freq=freq
-        self.__vie="vie"
+        self.__vie="vie" #Permet de savoir si le missile et en vie ou mort
         self.__camp=camp
         self.__image=self.__canevas.create_image(self.__posX,self.__posY,image=image,tags="missile"+str(numero))
         fenetre.after(100,self.fDeplacement_missile)
         
-        #Fonction permettant de retourner les positions du missile
+    """Fonction permettant de retourner les positions du missile"""
     def fGet(self):
         return (self.__posX, self.__posY)
-    
+
+    """Fonction permettant de définir si le méchant est mort"""
     def setmort(self):
         self.__vie="mort"
 
         
-        #Fonction permettant le déplacement du missile
+    """Fonction permettant le déplacement du missile et ses collisions"""
     def fDeplacement_missile(self):
         self.__posY += self.__vitesse
         self.__canevas.move(self.__image,0,self.__vitesse)
@@ -141,24 +145,27 @@ class cMissile():
             
             
             
-            
+    
 class cBlocks():
+    #Initialisation des blocks permettant se protéger
     def __init__(self,posX,posY,canevas,largeur,hauteur,numero):
         self.__posX=posX
         self.__posY=posY
         self.__canevas=canevas
         self.__numero=numero
         canevas.create_rectangle(posX-largeur/2,posY-hauteur/2,posX+largeur/2,posY+hauteur/2,fill=self.couleurandom(),tags="bloque"+str(numero))
-        
+    
+    """Fonction permettant d'attribuer une couleur aléatoire à chaque block,
+    la fonction renvoie une couleur en héxadécimal"""
     def couleurandom(self):
             couleur="#"
             l=["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"]
             for i in range(6):
                 couleur=couleur+l[randint(0,len(l)-1)]
             return couleur
-        
+    """Fonction permettant de retourner la position du block"""    
     def fGet(self):
             return (self.__posX,self.__posY)
-        
+    """Fonction permettant de supprimer un block après être touché"""    
     def setmort(self):
             self.__canevas.delete("bloque"+str(self.__numero))
